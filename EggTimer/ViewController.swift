c8d017fb0069f2b9898b69a2cafb0fd85d842c42
jasonliuhe <walkerliuhe@gmail.com>
2021-10-05 17:27:07 -0400

    Start

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let midiumTime = 7
+    let hardTime = 12
+    
+    @IBAction func eggButton(_ sender: UIButton) {
+        let hardNess = sender.currentTitle
+        
+        if (hardNess == "Soft") {
+            print(softTime)
+        } else if (hardNess == "Medium") {
+            print(midiumTime)
+        } else {
+            print(hardTime)
+        }
+    }
 }
